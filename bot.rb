d7cc38f93b163eef997ba881c7faf63d9a08842a
Michael Nipper <mjn4406@gmail.com>
2012-09-12 18:50:36 -0400

    Add credentials loading for IRC login

--- bot.rb
+++ bot.rb
@@ -1,10 +1,16 @@
 require 'cinch'
 Dir["./bots/*.rb"].each { |file| require file }
 
+## Get login credentials for IRC
+file = File.open("creds.txt", "r")
+credentials = file.read.delete!("\n")
+username = credentials.split("::")[0]
+password = credentials.split("::")[1]
+
 bot = Cinch::Bot.new do
   configure do |c|
-    c.server = ""
-    c.password = ""
+    c.server = username
+    c.password = password
     c.nick = "link"
     c.channels = ["#alaris_villains"]
     c.plugins.plugins = [GoogleBot, HelpBot, UrbanDictionary, TinyURL, SlapBot, CowsayBot]
