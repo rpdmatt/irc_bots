014c59af86865f7a51c1bfb41ed309a2fcdd6277
Michael Nipper <mjn4406@gmail.com>
2012-09-12 14:44:54 -0400

    Only slap or heal someone in the room

--- bots/slapbot.rb
+++ bots/slapbot.rb
@@ -13,19 +13,19 @@ class SlapBot
     end
 
     def slap(m, name)
-      unless name == bot.nick
+      if name != bot.nick && m.channel.has_user?(name)
 	@slaps.has_key?(name) ? @slaps[name] -= 10 : @slaps[name] = 90
 	m.reply("** slaps #{name} **")
 	m.reply("** #{name}'s health is #{@slaps[name]} **")
         if @slaps[name] <= 0
-	  Channel(@bot.channels.first).kick(name, "Too slapped up.")
+	  Channel(m.channel).kick(name, "Too slapped up.")
           @slaps.delete(name)
         end
       end
     end
 
     def heal(m, name)
-      unless name == m.user.nick
+      if name != bot.nick && m.channel.has_user?(name)
 	if @slaps.has_key?(name) && @slaps[name] < 100
           @slaps[name] += 10
 	  m.reply("** heals #{name} **")
