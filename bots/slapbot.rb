c803d70396d99c7ee9f6c11bd98920a10080a64b
Michael Nipper <mjn4406@gmail.com>
2012-09-12 19:18:50 -0400

    Case insensitive slapping

--- bots/slapbot.rb
+++ bots/slapbot.rb
@@ -14,29 +14,29 @@ class SlapBot
 
     def slap(m, name)
       if name != bot.nick && m.channel.has_user?(name)
-	@slaps.has_key?(name) ? @slaps[name] -= 10 : @slaps[name] = 90
+	@slaps.has_key?(name.downcase) ? @slaps[name.downcase] -= 10 : @slaps[name.downcase] = 90
 	m.reply("** slaps #{name} **")
-	m.reply("** #{name}'s health is #{@slaps[name]} **")
-        if @slaps[name] <= 0
+	m.reply("** #{name}'s health is #{@slaps[name.downcase]} **")
+        if @slaps[name.downcase] <= 0
 	  Channel(m.channel).kick(name, "Too slapped up.")
-          @slaps.delete(name)
+          @slaps.delete(name.downcase)
         end
       end
     end
 
     def heal(m, name)
       if name != bot.nick && m.channel.has_user?(name)
-	if @slaps.has_key?(name) && @slaps[name] < 100
-          @slaps[name] += 10
+	if @slaps.has_key?(name.downcase) && @slaps[name.downcase] < 100
+          @slaps[name.downcase] += 10
 	  m.reply("** heals #{name} **")
-	  m.reply("** #{name}'s health is #{@slaps[name]} **")
+	  m.reply("** #{name}'s health is #{@slaps[name.downcase]} **")
         end
       end
     end
 
     def hp(m)
-      if @slaps.has_key?(m.user.to_s)
-        m.reply("** #{m.user}'s hp is #{@slaps[m.user.to_s]} **")
+      if @slaps.has_key?(m.user.to_s.downcase)
+        m.reply("** #{m.user}'s hp is #{@slaps[m.user.to_s.downcase]} **")
       else
         m.reply("** #{m.user}'s hp is 100 **")
       end
