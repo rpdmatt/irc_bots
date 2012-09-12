2cf6cd9c7b6cb587985cabd97802f2e624568b77
Michael Nipper <mjn4406@gmail.com>
2012-09-12 19:40:56 -0400

    Update help

--- bots/helpbot.rb
+++ bots/helpbot.rb
@@ -8,6 +8,10 @@ class HelpBot
     def execute(m)
       m.reply("!google [query] - Get link to first google result for query")
       m.reply("!urban [word] - Get defintion from urban dictionary")
+      m.reply("!slap [user] - Slap a user")
+      m.reply("!heal [user] - Heal a user")
+      m.reply("!hp - Get your current hp")
+      m.reply("!cowsay [something] - Make a cow say something!")
     end
 
 end
