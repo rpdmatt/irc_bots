d8d3e8fd6fc6a5e6dc62a7a37ee18e45f24aca01
Michael Nipper <mjn4406@gmail.com>
2012-09-17 16:20:18 -0400

    Update roulette bot to op after 6 tries

--- bots/roulettebot.rb
+++ bots/roulettebot.rb
@@ -5,8 +5,20 @@ class RouletteBot
 
     match /roulette/, method: :roulette
 
+    def initialize(*args)
+      super
+      @results = Hash.new
+    end
+
     def roulette(m)
-      Random.rand(6) == 5 ? Channel(m.channel).kick(m.user.nick, "Lost russian roulette.") : m.reply("** #{m.user.nick} survived russian roulette **")
+      if Random.rand(6) == 5
+        Channel(m.channel).kick(m.user.nick, "Lost russian roulette.")
+        @results.delete(m.user.nick.downcase)
+      else 
+        m.reply("** #{m.user.nick} survived russian roulette **")
+        @results.has_key?(m.user.nick.downcase) ? @results[m.user.nick.downcase] += 1 : @results[m.user.nick.downcase] = 1
+        m.channel.op(m.user.nick) if @results[m.user.nick.downcase] == 6
+      end
     end
 
 end
