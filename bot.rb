3e37395529d4574910978cb4a82414f960b446fc
Matthew McClory <rpdmatt@aol.com>
2012-09-27 21:05:57 -0400

    Added Lyrics Bot

--- bot.rb
+++ bot.rb
@@ -4,16 +4,17 @@ Dir["./bots/*.rb"].each { |file| require file }
 
 bot = Cinch::Bot.new do
   configure do |c|
+    c.port = Creds.port
     c.server = Creds.server
     c.password = Creds.password
     c.nick = Creds.nick
     c.channels = Creds.channels
-    c.plugins.plugins = [GoogleBot, HelpBot, UrbanDictionary, SlapBot, CowsayBot, OpBot, AsciiBot, RouletteBot]
+    c.plugins.plugins = [TBS]
   end
 
   ["hello", "hey", "hi", "howdy"].each do |greeting|
     on :message, greeting do |m|
-      m.reply "Howdy, #{m.user.nick}!  Type !help for a list of bot commands."
+      m.reply "Have you seen Han Solo?"
     end
   end
 
---
+++ bots/lyricstest.rb
@@ -0,0 +1,35 @@
+require 'nokogiri'
+require 'open-uri'
+require 'cinch'
+
+class TBS
+	include Cinch::Plugin
+
+		match /tbs/
+
+		def execute(m)
+		url = "http://www.metrolyrics.com/taking-back-sunday-lyrics.html"
+
+		doc = Nokogiri::HTML(open(url))
+		count = 0
+		a = Array.new
+		doc.css(".song").each do |item|
+			a.push 'http://www.metrolyrics.com' + item.at_css("a")[:href]
+		end
+
+		r = (rand()*a.length).to_i + 1
+		#puts r
+		#puts r
+		#puts a.length
+
+		newdoc = Nokogiri::HTML(open(a[r]))
+		b = Array.new
+		lyricdoc = newdoc.xpath(".//div[@id='lyrics-body']/p")
+
+
+		lyricdoc.xpath(".//span[count(preceding::br)=#{rand(lyricdoc.xpath(".//br").length)}]").each do |item|
+			m.reply(item.text)
+		end
+
+		end
+	end
